9ddbf9cad55323dec0d2fc759e113404be5630e7
Leo Mckee-Reid <leo.tzu.mr@gmail.com>
2021-07-15 04:14:26 -0700

    added largest coefficient pivot method, though bugs still exist

--- LP_solver.py
+++ LP_solver.py
@@ -14,36 +14,43 @@ def parse_input():
 	for iter, line in enumerate(lines):
 		#print(line)
 		if iter is 0:
-			obj_fun = np.array([float(x.strip()) for x in line.split()])
-			# obj_fun *= -1 # this is necessary for running the scipy's linprog
-			num_vars = len(obj_fun)
-			num_constraints = len(lines)
-			# declare lhs and rhs matrices with proper dimensions  
-			lhs = np.zeros(shape=(num_constraints-1, num_vars))
-			rhs = np.zeros(shape=(num_constraints-1, 1))
+			obj = np.array([float(x.strip()) for x in line.split()])
+			# obj *= -1 # this is only necessary for running the scipy's linprog
+			num_vars = len(obj)
+			num_constraints = len(lines)-1
+			# declare lhs and rhs matrices with proper dimensions: 
+			lhs = np.zeros(shape=(num_constraints, num_vars))
+			rhs = np.zeros(shape=(num_constraints, 1))
+			# initialize objective function variables: x1=1, x2=2, ..., xn=n
+			obj_vars = np.array([x+1 for x in range(num_vars)])
+			# initialize basis variables (lhs_vars): w1=-1, w2=-2, ... ,wn=-n
+			lhs_vars = np.zeros(shape=(num_constraints, num_vars+1))	
+			for w in range(num_constraints):
+				lhs_vars[w] = np.array([x for x in range(num_vars+1)])
+				lhs_vars[w,0] = -(w+1)
 			continue
-
-		# for every line in the txt file, fill in that row for both lhs and rhs (Apparently this is the efficient numpy way)
+		# for every line in the txt file, fill in that row for both lhs and rhs (this is the efficient numpy way)
 		lhs[iter-1] = np.array([float(x.strip()) for x in line.split()[:num_vars]])		
-		rhs[iter-1] = np.array([float(x.strip()) for x in line.split()[num_vars:]])		
+		rhs[iter-1] = np.array([float(x.strip()) for x in line.split()[num_vars:]])	
 		
 	#'''
 	# print out the parsed data for bug inspection	
 	print("==================================")
-	print("objective function:\n", obj_fun)
+	print("objective function:\n", obj)
 	print("==================================")
 	print("Left hand side:\n", lhs)
 	print("==================================")
 	print("Constants (rhs):\n", rhs)
 	print("==================================")
 	#'''
+
 	f.close()
-	return obj_fun, lhs, rhs
+	return obj, obj_vars, lhs, lhs_vars, rhs
 
 # Determine if the dictionary is at an optimal solution
-def is_optimal(obj_fun):
+def is_optimal(obj):
 	# If all vals in obj_fun are negative, return true
-	for i in obj_fun:
+	for i in obj:
 		if i > 0:
 			return False
 	return True
@@ -60,19 +67,19 @@ def is_degenerate():
 	# TODO: if there are 2 zeros in rhs, return true
 	return False
 
-def lexicographic_method(obj_fun, lhs, rhs):
+def lexicographic_method(obj, lhs, rhs):
 	# TODO: how to do this symbolically??
 	return rhs
 
-def largest_coeff_pivot(obj_fun, lhs, rhs):
+def largest_coeff_pivot(obj, lhs, rhs):
 	# TODO: find largest coeff, pivot it, return 
-	# choose the largest positive obj_fun coeff. if all basis function coeffs in that col are > 0, then print_unbounded()
+	# choose the largest positive obj coeff. if all basis function coeffs in that col are > 0, then print_unbounded()
 	
 	# find entering variable:
 	max_var = 0
 	# initialized to -1 so that if no entering var is found, an error can be called
 	entering_var = -1
-	for pos, elem in enumerate(obj_fun):
+	for pos, elem in enumerate(obj):
 		if elem > max_entering:
 			max_var = elem
 			entering_var = pos
@@ -86,10 +93,10 @@ def largest_coeff_pivot(obj_fun, lhs, rhs):
 				min_ratio = rhs[pos]/lhs[pos]
 				leaving_var = pos
 	
-	# now perform pivot:
-	# TODO:
+	# perform pivot:
 	
-	return obj_fun, lhs, rhs
+	
+	return obj, lhs, rhs
 
 def use_linprog(obj_fun, lhs, rhs):
 	# use linprog() to calculate solution
@@ -117,22 +124,23 @@ def use_linprog(obj_fun, lhs, rhs):
 		print("UNKNOWN ERROR")
 
 def main():
-	
+
 	if len(sys.argv) != 2:
 		print("Error: this program requires exactly 1 argument -> an input file containing the linear program to be solved.")
 		print("-----> Example of correct program call: $ python LP_solver.py input_file.txt")
 		return 0
 	
 	# parses input file into numpy arrays
-	obj_fun, lhs, rhs = parse_input()
+	obj, obj_vars, lhs, lhs_vars, rhs = parse_input()	
+	# initialize objective value to 0
+	obj_val = 0
 	
 	'''
 	# use scipy's linear program solver (solves 29/32 test cases) 	
-	use_linprog(obj_fun, lhs, rhs)
+	use_linprog(obj, lhs, rhs)
 	'''
-	return 0
 	
-	while not is_optimal():
+	while not is_optimal(obj):
 		''' # these are both detected while running largest_coeff_pivot()
 		if is_infeasible():
 			print('infeasible')
@@ -141,9 +149,96 @@ def main():
 			print('unbounded')
 			return 0
 		'''
+		'''
 		if is_degenerate():
-			rhs = lexicographic_method(obj_fun, lhs, rhs)
-		obj_fun, lhs, rhs = largest_coeff_pivot(obj_fun, lhs, rhs)
+			rhs = lexicographic_method(obj, lhs, rhs)
+		'''
+		#obj, obj_vars, obj_val, lhs, lhs_vars, rhs = largest_coeff_pivot(obj, obj_vars, obj_val, lhs, lhs_vars, rhs) 
+		# ***************************************************************
+		# TODO: LARGEST_COEFF_PIVOT: find largest coeff, pivot it, return 
+		# choose the largest positive obj coeff. if all basis function coeffs in that col are > 0, then print_unbounded()
+	
+		# find entering variable:
+		max_coeff = 0
+		# initialized to -1 so that if no entering position is found, an error can be called
+		entering_pos = -1
+		for pos, elem in enumerate(obj):
+			if elem > max_coeff:
+				max_coeff = elem
+				entering_pos = pos
+				print('new entering_pos: ', entering_pos)
+
+		# find leaving variable:
+		min_ratio = np.inf # numpy infinity
+		leaving_pos = -1
+		###################
+		for pos in range(np.shape(lhs)[0]):
+			if lhs[pos, entering_pos] > 0 and rhs[pos] > 0:
+				if rhs[pos] / lhs[pos,entering_pos] < min_ratio:
+					min_ratio = rhs[pos] / lhs[pos, entering_pos]
+					leaving_pos = pos
+					print('new leaving_pos: ', leaving_pos)
+					print('new min_ratio', min_ratio)
+		print('entering_pos: ', entering_pos)
+		print('leaving_pos: ', leaving_pos)
+		print('***************')
+		print(obj)
+		print(lhs)
+		# perform pivot (2 parts: 1. pivot variable placement; 2. update coefficients):
+		# 1. pivot just the variable location arrays:
+		temp_var = 0
+		for pos in range(len(obj_vars)):
+			if pos == entering_pos:
+				temp_var = obj_vars[pos]
+				obj_vars[pos] = lhs_vars[leaving_pos,0]
+		for row_num, row in enumerate(lhs_vars):           #range(np.shape(lhs_vars)[0]):
+			for col_num, val in enumerate(row):
+				if col_num == 0 and row_num == leaving_pos:
+					lhs_vars[row_num, col_num] = temp_var
+				if col_num == entering_pos+1:
+					lhs_vars[row_num, col_num] = obj_vars[entering_pos]
+		
+		#__import__('code').interact(local={k: v for ns in (globals(), locals()) for k, v in ns.items()})
+		
+		# 2. update coefficients:
+		
+		# objective value
+		obj_val += obj[entering_pos] * rhs[leaving_pos] / lhs[leaving_pos, entering_pos]	
+	
+		# this is a special coefficient that is used a lot, so it gets a special name
+		pivot_coeff = lhs[leaving_pos, entering_pos]
+		# objection function coefficients:
+		for pos in range(len(obj)):
+			if pos == entering_pos:
+				obj[pos] *= -1/pivot_coeff
+			obj[pos] -= obj[entering_pos] * lhs[leaving_pos, pos] / pivot_coeff
+
+		# lhs and rhs coefficients:
+		for row_num, row in enumerate(lhs):
+			for col_num, val in enumerate(row):
+				if row_num == leaving_pos:
+					if col_num == entering_pos:
+						lhs[row_num, col_num] = 1 / pivot_coeff
+					lhs[row_num, col_num] /= pivot_coeff
+		rhs[leaving_pos] /= pivot_coeff
+		# second pass through the matrix allows us to use the previously calculated coefficients
+		for row_num, row in enumerate(lhs):
+			# to ensure we don't change this value before we're done referencing it
+			row_coeff = lhs[row_num, entering_pos]
+			if row_num == leaving_pos:
+				continue
+			rhs[row_num] += row_coeff * rhs[leaving_pos]
+			for col_num, val in enumerate(row):
+				if col_num == entering_pos:
+					lhs[row_num, col_num] /= -pivot_coeff
+				lhs[row_num, col_num] -= row_coeff * lhs[leaving_pos, col_num]
+				rhs[row_num]
+	print('final objective coefficients:')
+	print(obj)
+	print('final obj_vars:')
+	print(obj_vars)
+	print('optimal')
+	print(obj_val)		
 
 	return 0
 
